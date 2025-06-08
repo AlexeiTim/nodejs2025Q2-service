e3d7f7a1409625b46648eb5e8fcd30af75a57d00
timashkov <timashkov060896@gmail.com>
2025-06-08 11:54:55 +0300

    feat/add-prisma

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20.11-alpine AS builder
+FROM node:22.14.0-alpine AS builder
 
 WORKDIR /app
 
@@ -11,16 +11,23 @@ RUN npm audit || true
 
 COPY . .
 
+# Generate Prisma Client
+RUN npx prisma generate
+
 # Add build step to compile TypeScript
 RUN npm run build
 
-FROM node:20.11-alpine
+FROM node:22.14.0-alpine
 
 WORKDIR /app
 
 COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/dist ./dist
 COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/prisma ./prisma
+
+# Generate Prisma Client in production image
+RUN npx prisma generate
 
 EXPOSE 4000
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
 import { FavoriteModule } from './favorite/favorite.module';
-import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    PrismaModule,
+    DatabaseModule,
     UserModule,
-    TrackModule,
     ArtistModule,
     AlbumModule,
+    TrackModule,
     FavoriteModule,
-    ConfigModule.forRoot(),
-    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
